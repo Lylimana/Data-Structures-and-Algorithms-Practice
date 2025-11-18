dc5cfacba4c0ca4409da31c48107aa3766048344
Miguel Manalili <manalili.mig@gmail.com>
2025-11-18 01:49:59 +0000

    linked list merge sort

--- Lesson 3/linked_list.py
+++ Lesson 3/linked_list.py
@@ -136,7 +136,20 @@ class LinkedList:
             current = current.next_node
                 
             previous.next_node = current.next_node
-        
+    
+    def node_at_index(self, index):
+        if index == 0: 
+            return self.head
+        else: 
+            current = self.head
+            position = 0 
+            
+            while position < index:
+                current = current.next_node
+                position += 1
+                
+        return current 
+     
     def __repr__(self): 
         '''
         Return a string representation of the list
--- Lesson 3/practice.py
+++ Lesson 3/practice.py
@@ -1,4 +1,5 @@
 from linked_list import Node, LinkedList
+from linked_list_merge_sort import merge_sort
 
 N1 = Node(10)
 
@@ -29,3 +30,19 @@ linkedlist.remove_index(5)
 
 print(linkedlist)
 
+linkedlist1 = LinkedList()
+
+linkedlist1.add(2)
+linkedlist1.add(24)
+linkedlist1.add(46)
+linkedlist1.add(68)
+linkedlist1.add(81)
+linkedlist1.add(1)
+linkedlist1.add(35)
+linkedlist1.add(57)
+linkedlist1.add(79)
+linkedlist1.add(92)
+
+print(linkedlist1)
+
+print(merge_sort(linkedlist1))
